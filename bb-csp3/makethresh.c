ff4ac8cbc4110bb753b8f645b104d3f15e162314
BB <babennet+github@ualberta.ca>
2013-06-19 17:03:52 -0600

    Adopted new code from Rupam

--- bb-csp3/makethresh.c
+++ bb-csp3/makethresh.c
@@ -75,7 +75,7 @@ int main(int argc, char *argv[]) {
       exit(EXIT_FAILURE);
     }
     fprintf(fd, "1\n");
-    for (i = 0; i < 4; i++) fprintf(fd, "%d\n", (outsideMin[i]-insideMax[i])/2);
+    for (i = 0; i < 4; i++) fprintf(fd, "%d\n", (outsideMin[i]+insideMax[i])/2);
     fclose(fd);
     printf("Success! Margin is %d (inside is darker/lower)\n", minMargin);
     return 0;
@@ -90,7 +90,7 @@ int main(int argc, char *argv[]) {
       exit(EXIT_FAILURE);
     }
     fprintf(fd, "0\n");
-    for (i = 0; i < 4; i++) fprintf(fd, "%d\n", (insideMin[i]-outsideMax[i])/2);
+    for (i = 0; i < 4; i++) fprintf(fd, "%d\n", (insideMin[i]+outsideMax[i])/2);
     printf("Success! Margin is %d (inside is lighter/higher)\n", minMargin);
     fclose(fd);
     return 0;
@@ -100,6 +100,4 @@ int main(int argc, char *argv[]) {
     printf("Margin %d too small; needs to be %d\n", minMargin, REQUIRED_MARGIN);
   else
     printf("Bounds overlap by %d\n", -minMargin);
-  // TODO: Improve program termination
-  return 0;
 }
