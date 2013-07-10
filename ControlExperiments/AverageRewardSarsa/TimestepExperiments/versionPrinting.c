53a541229a8bf2817b598099ed43ed6432d477cc
BB <babennet+github@ualberta.ca>
2013-07-09 21:07:58 -0600

    Added some version printing related stuff

--- ControlExperiments/AverageRewardSarsa/TimestepExperiments/versionPrinting.c
+++ ControlExperiments/AverageRewardSarsa/TimestepExperiments/versionPrinting.c
@@ -2,9 +2,24 @@
 #include <stdlib.h>
 
 
+// Use C preprocessor macros to help automate logging the system and version
+// that the code was run on.
+#ifdef _WIN32
+//Windows
+#elif __unix__ // all unices
+//Unix
+#elif __posix__
+//Posix
+#elif __linux__
+//Linux
+#elif __APPLE__
+//Apple
+#endif
 
 #ifdef _WIN32
 #define __OSPLATFORM__ "Win32"
+#elif __posix__
+#define __OSPLATFORM__ "Posix"
 #elif __linux__
 #define __OSPLATFORM__ "Linux"
 #elif __APPLE__
