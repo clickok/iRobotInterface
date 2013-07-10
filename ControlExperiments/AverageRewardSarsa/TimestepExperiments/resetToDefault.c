87ab3e650b752eaef8618f44d8128e595e16ad2b
BB <babennet+github@ualberta.ca>
2013-07-09 21:00:45 -0600

    Minor changes

--- ControlExperiments/AverageRewardSarsa/TimestepExperiments/resetToDefault.c
+++ ControlExperiments/AverageRewardSarsa/TimestepExperiments/resetToDefault.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef _WIN32
+#define __OSPLATFORM__ "Win32"
+#elif __unix__ // all unices
+#define __OSPLATFORM__ "Unix"
+#elif __posix__
+    // POSIX
+#elif __linux__
+    // linux
+#elif __APPLE__
+    // Mac OS, not sure if this is covered by __posix__ and/or __unix__ though...
+#endif
+
 int main()
 {
 	printf("Version: %s\n",__VERSION__);
