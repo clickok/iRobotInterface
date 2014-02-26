2ef940aff3cb7a851ff8754b6692905bc44cc811
clickok <babennet@ualberta.ca>
2014-02-26 00:06:29 -0700

    minor changes

--- csp3bb-v0.95/edgeBackwards.c
+++ csp3bb-v0.95/edgeBackwards.c
@@ -179,7 +179,7 @@ int main(int argc, char *argv[]) {
       reward = -5;
     }
     // Add a negative reward for being off the edge
-    reward -= 5 * (sCliffLB[p] + sCliffFLB[p] + sCliffFRB[p] + sCliffRB[p])
+    reward -= 5 * (sCliffLB[p] + sCliffFLB[p] + sCliffFRB[p] + sCliffRB[p]);
 
     rewardReport += reward;
     if (rewardReport > 50) {
