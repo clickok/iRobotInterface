317f1d8def1b83d964926d85a7396897c19beff5
clickok <babennet@ualberta.ca>
2014-02-26 00:22:39 -0700

    minor changes

--- csp3bb-v0.95/edgeBackwards.c
+++ csp3bb-v0.95/edgeBackwards.c
@@ -201,7 +201,7 @@ int main(int argc, char *argv[]) {
     for (j = 0; j < 4; j++)
       e[s][j] = 0;
     e[s][a] = 1;
-    printf("s a r s' a': %d\t%d\t%d\t%d\t%d\n", s, a, reward, sprime, aprime);
+    printf("s a r s' a': %d \t %d \t %d \t %d \t %d\n", s, a, reward, sprime, aprime);
     for (i = 0; i < 16; i++) {
       printf("Action values for state %d: %f %f %f %f\n",i, Q[i][0], Q[i][1], Q[i][2], Q[i][3]);
       printf("Eligibility traces for state %d: %f %f %f %f\n", i, e[i][0], e[i][1], e[i][2], e[i][3]);
