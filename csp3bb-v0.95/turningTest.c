40ef48e45c5f891d33386a33f92246ef34cb88ae
clickok <babennet@ualberta.ca>
2014-02-27 00:40:24 -0700

    minor changes

--- csp3bb-v0.95/turningTest.c
+++ csp3bb-v0.95/turningTest.c
@@ -166,6 +166,7 @@ int main(int argc, char *argv[]) {
 
     // Begin executing turn
     p = ((getPktNum() + M - 1) % M);
+    printf("p = %d\n");
 
     // Determine whether IR cliff sensors are on terrain or off
     int tmp = 0;
