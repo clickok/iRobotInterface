b11b5edee44f5db9d7a18e7f79763eacc10f2de3
clickok <babennet@ualberta.ca>
2014-03-17 01:56:35 -0600

    Removed dependency on SensorPackets.json

--- py3-csp3/cspy3.py
+++ py3-csp3/cspy3.py
@@ -1,3 +1,25 @@
+#python3
+"""
+cspy3.py version 0.1
+####################
+
+This program was written in order to communicate with and control an iRobot Create
+in a variety of different ways-- directly from a computer or over SSH, for example.
+
+It is neither complete nor perfect, but suffices for moving the Create around and 
+getting access to certain sensors without having to write a separate program.
+
+Usage:
+	python cspy3.py <serial_port> <pckt_id_#1> [<pckt_id_#2>, [...]]
+
+Example:
+	#(Battery Information)
+	python cspy3.py /dev/ttyUSB0 24 25 26 
+
+
+--bab 2014 
+"""
+
 import array
 import json
 import numpy as np 
@@ -13,6 +35,7 @@ import tty
 
 from time import sleep
 
+# A partial enumeration of opcodes (necessary to command the Create to do things)
 OP_PASSIVE = 128
 OP_CONTROL = 130
 OP_FULL = 132
@@ -30,6 +53,226 @@ OP_STREAM = 148
 OP_PAUSE  = 150
 OP_QUERY  = 142
 OP_QUERY_LIST = 149
+
+# The sensor packets are described in a dict, based on the Open Interface spec
+
+PacketDct = \
+{7: {'ValueRange': [0, 31],
+  'dtype': 'B',
+  'id': 7,
+  'name': 'BumpsAndWheelDrops',
+  'size': 1,
+  'units': None},
+ 8: {'ValueRange': [0, 1],
+  'dtype': 'B',
+  'id': 8,
+  'name': 'Wall',
+  'size': 1,
+  'units': None},
+ 9: {'ValueRange': [0, 1],
+  'dtype': 'B',
+  'id': 9,
+  'name': 'CliffLeft',
+  'size': 1,
+  'units': None},
+ 10: {'ValueRange': [0, 1],
+  'dtype': 'B',
+  'id': 10,
+  'name': 'CliffFrontLeft',
+  'size': 1,
+  'units': None},
+ 11: {'ValueRange': [0, 1],
+  'dtype': 'B',
+  'id': 11,
+  'name': 'CliffFrontRight',
+  'size': 1,
+  'units': None},
+ 12: {'ValueRange': [0, 1],
+  'dtype': 'B',
+  'id': 12,
+  'name': 'CliffRight',
+  'size': 1,
+  'units': None},
+ 13: {'ValueRange': [0, 1],
+  'dtype': 'B',
+  'id': 13,
+  'name': 'VirtualWall',
+  'size': 1,
+  'units': None},
+ 14: {'ValueRange': [0, 31],
+  'dtype': 'B',
+  'id': 14,
+  'name': 'Overcurrents',
+  'size': 1,
+  'units': None},
+ 15: {'ValueRange': [0, 0],
+  'dtype': 'B',
+  'id': 15,
+  'name': 'Unused',
+  'size': 1,
+  'units': None},
+ 16: {'ValueRange': [0, 0],
+  'dtype': 'B',
+  'id': 16,
+  'name': 'Unused',
+  'size': 1,
+  'units': None},
+ 17: {'ValueRange': [0, 255],
+  'dtype': 'B',
+  'id': 17,
+  'name': 'IRByte',
+  'size': 1,
+  'units': None},
+ 18: {'ValueRange': [0, 15],
+  'dtype': 'B',
+  'id': 18,
+  'name': 'Buttons',
+  'size': 1,
+  'units': None},
+ 19: {'ValueRange': [-32768, 32767],
+  'dtype': 'h',
+  'id': 19,
+  'name': 'Distance',
+  'size': 2,
+  'units': None},
+ 20: {'ValueRange': [0, 5],
+  'dtype': 'h',
+  'id': 20,
+  'name': 'Angle',
+  'size': 2,
+  'units': None},
+ 21: {'ValueRange': [0, 65535],
+  'dtype': 'B',
+  'id': 21,
+  'name': 'ChargingState',
+  'size': 1,
+  'units': None},
+ 22: {'ValueRange': [-37268, 37268],
+  'dtype': 'H',
+  'id': 22,
+  'name': 'Voltage',
+  'size': 2,
+  'units': None},
+ 23: {'ValueRange': [-128, 127],
+  'dtype': 'h',
+  'id': 23,
+  'name': 'Current',
+  'size': 2,
+  'units': None},
+ 24: {'ValueRange': [0, 65535],
+  'dtype': 'b',
+  'id': 24,
+  'name': 'BatteryTemperature',
+  'size': 1,
+  'units': None},
+ 25: {'ValueRange': [0, 65535],
+  'dtype': 'H',
+  'id': 25,
+  'name': 'BatteryCharge',
+  'size': 2,
+  'units': None},
+ 26: {'ValueRange': [0, 4095],
+  'dtype': 'H',
+  'id': 26,
+  'name': 'BatteryCapacity',
+  'size': 2,
+  'units': None},
+ 27: {'ValueRange': [0, 4095],
+  'dtype': 'H',
+  'id': 27,
+  'name': 'WallSignal',
+  'size': 2,
+  'units': None},
+ 28: {'ValueRange': [0, 4095],
+  'dtype': 'H',
+  'id': 28,
+  'name': 'CliffLeftSignal',
+  'size': 2,
+  'units': None},
+ 29: {'ValueRange': [0, 4095],
+  'dtype': 'H',
+  'id': 29,
+  'name': 'CliffFrontLeftSignal',
+  'size': 2,
+  'units': None},
+ 30: {'ValueRange': [0, 4095],
+  'dtype': 'H',
+  'id': 30,
+  'name': 'CliffFrontRightSignal',
+  'size': 2,
+  'units': None},
+ 31: {'ValueRange': [0, 4095],
+  'dtype': 'H',
+  'id': 31,
+  'name': 'CliffRightSignal',
+  'size': 2,
+  'units': None},
+ 32: {'ValueRange': [0, 31],
+  'dtype': 'B',
+  'id': 32,
+  'name': 'UserDigitalInputs',
+  'size': 1,
+  'units': None},
+ 33: {'ValueRange': [0, 1023],
+  'dtype': 'B',
+  'id': 33,
+  'name': 'UserAnalogInput',
+  'size': 2,
+  'units': None},
+ 34: {'ValueRange': [0, 3],
+  'dtype': 'B',
+  'id': 34,
+  'name': 'ChargingSourcesAvailable',
+  'size': 1,
+  'units': None},
+ 35: {'ValueRange': [0, 3],
+  'dtype': 'B',
+  'id': 35,
+  'name': 'OIMode',
+  'size': 1,
+  'units': None},
+ 36: {'ValueRange': [0, 15],
+  'dtype': 'B',
+  'id': 36,
+  'name': 'SongNumber',
+  'size': 1,
+  'units': None},
+ 37: {'ValueRange': [0, 1],
+  'dtype': 'B',
+  'id': 37,
+  'name': 'SongPlaying',
+  'size': 1,
+  'units': None},
+ 38: {'ValueRange': [0, 42],
+  'dtype': 'B',
+  'id': 38,
+  'name': 'NumberOfStreamPackets',
+  'size': 1,
+  'units': None},
+ 39: {'ValueRange': [-500, 500],
+  'dtype': 'h',
+  'id': 39,
+  'name': 'Velocity',
+  'size': 2,
+  'units': None},
+ 40: {'ValueRange': [-32768, 32767],
+  'dtype': 'h',
+  'id': 40,
+  'name': 'Radius',
+  'size': 2,
+  'units': None},
+ 41: {'ValueRange': [-500, 500],
+  'dtype': 'h',
+  'id': 41,
+  'name': 'RightVelocity',
+  'size': 2,
+  'units': None},
+ 42: {'ValueRange': [-500, 500],
+  'dtype': 'h',
+  'id': 42,
+  'name': 'LeftVelocity',
+  'size': 2,
+  'units': None}}
 
 
 SERIAL_PARAMS = {"baudrate":    57600,
@@ -317,11 +560,11 @@ def main():
 
 	#oldCWD = os.getcwd()
 	#os.chdir(os.chdir(os.path.dirname(sys.argv[0])))
-	with open("SensorPackets.json", "r") as f:
-		tmpDct = json.load(f)
-		# Handle the fact that JSON cannot have integer keys
-		global PacketDct
-		PacketDct = {int(k):v for k, v in tmpDct.items()}
+	# with open("SensorPackets.json", "r") as f:
+	# 	tmpDct = json.load(f)
+	# 	# Handle the fact that JSON cannot have integer keys
+	# 	global PacketDct
+	# 	PacketDct = {int(k):v for k, v in tmpDct.items()}
 	#os.chdir(oldCWD)
 
 
